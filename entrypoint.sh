19b19c9eb87041e5c0a453239284383e2157c3db
Aman Setia <amansetiarjp@gmail.com>
2021-12-20 13:30:14 +0530

    curl output to file fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rclone version
 #rclone config create CLOUDNAME mega user=$UserName password=$PassWord
-curl $UserName -o /.config/rclone/rclone.config
+curl $UserName >> /.config/rclone/rclone.config
 rclone version
 rclone serve http $PassWord: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes
 
