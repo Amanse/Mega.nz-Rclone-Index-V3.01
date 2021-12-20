29cf2610c4ddad78c8b99e34b2318e78d006c161
Aman Setia <amansetiarjp@gmail.com>
2021-12-20 13:37:43 +0530

    Rclone please work

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 rclone version
 #rclone config create CLOUDNAME mega user=$UserName password=$PassWord
-curl $UserName >> /.config/rclone/rclone.config
+mkdir /rclone
+curl $UserName >> /rclone/rclone.config
+rclone --config=/rclone/rclone.config
 rclone version
 rclone serve http $PassWord: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes
 
