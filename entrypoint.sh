22bcdedd7867a30420a33592af248c8f508a2964
Aman Setia <amansetiarjp@gmail.com>
2021-12-20 13:28:51 +0530

    Curl the config, use env variable for config name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 rclone version
-rclone config create CLOUDNAME mega user=$UserName password=$PassWord
+#rclone config create CLOUDNAME mega user=$UserName password=$PassWord
+curl $UserName -o /.config/rclone/rclone.config
 rclone version
-rclone serve http CLOUDNAME: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes
+rclone serve http $PassWord: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes
 
 
