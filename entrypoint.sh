61db9361209b548beb4d10de2c39bf4cf1715562
Aman Setia <amansetiarjp@gmail.com>
2021-12-20 13:24:29 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rclone version
-rclone config create 'CLOUDNAME' 'mega' 'user' $UserName 'pass' $PassWord
+rclone config create CLOUDNAME mega user=$UserName password=$PassWord
 rclone version
 rclone serve http CLOUDNAME: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes
 
